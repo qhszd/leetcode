76b970f59530b38299e14171b8274fcca4943ffc
shaozhd <shaozhd@mail2.sysu.edu.cn>
2022-10-07 10:45:01 +0800

    20221007

--- 2022/Oct/7.py
+++ 2022/Oct/7.py
@@ -1,8 +1,8 @@
 # #!/usr/bin/python3
-# # -*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 
-# # Description
-# # 1800. Maximum Ascending Subarray Sum
+# Description
+# 1800. Maximum Ascending Subarray Sum
 
 # Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
 # A subarray is defined as a contiguous sequence of numbers in an array.
